536da7525059dea44dc879f96a0c9fa70f47aba9
Doc077 <dom.rajkowski@gmail.com>
2022-07-09 20:47:52 +0200

    Add createServer function

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,3 @@
 export * from './config/env.function';
+export * from './server/functions/create-server.function';
 export * from './server/server.class';
---
+++ src/server/functions/create-server.function.ts
@@ -0,0 +1,5 @@
+import { Server } from '../server.class';
+
+export const createServer = () => {
+  return new Server();
+}
