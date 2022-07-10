6428ba7368ad85cf86e353ac8b5c944fe8011663
Doc077 <dom.rajkowski@gmail.com>
2022-07-10 21:18:10 +0200

    Add basic view engine and HTTP status page

--- src/server/server.class.ts
+++ src/server/server.class.ts
@@ -1,9 +1,11 @@
+import * as constants from '../constants';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { env } from '../config/env.function';
+import { encode } from 'html-entities';
 import { exec } from 'child_process';
-import { existsSync, unlinkSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'fs';
 import express, { Request, Response } from 'express';
 import methodOverride from 'method-override';
 import session from 'express-session';
@@ -44,9 +46,40 @@ export class Server {
 
     const app = express();
 
+    app.engine('atom.html', (filePath: string, variables: Record<string, any>, callback) => {
+      let compiled = readFileSync(filePath).toString();
+
+      for (const expression of compiled.matchAll(/\{([a-zA-Z0-9]*?)\}/g) ?? []) {
+        const name: string = expression[1];
+        const isConstant = name.startsWith('NUCLEON_') || name.startsWith('NODE_');
+  
+        let variableValue: string = isConstant
+          ? constants[name as keyof object]
+          : variables[name];
+  
+        if (isConstant && !(name in constants)) {
+          throw new Error(`The '${name}' constant is not defined`);
+        }
+  
+        if (!isConstant && !(name in variables)) {
+          throw new Error(`The '${name}' variable has not been passed to the view`);
+        }
+  
+        variableValue = Array.isArray(variableValue) || typeof variableValue === 'object'
+          ? JSON.stringify(variableValue)
+          : encode(String(variableValue));
+  
+        compiled = compiled.replace(expression[0], variableValue);
+      }
+
+      return callback(null, compiled);
+    });
+
     app.set('trust proxy', 1);
     app.set('views', 'views');
+    app.set('view engine', 'atom.html');
 
+    app.use(cookieParser());
     app.use(bodyParser.json());
 
     app.use(methodOverride((request: Request) => {
@@ -75,7 +108,11 @@ export class Server {
 
     app.all('*', async (request: Request, response: Response) => {
       response.status(404);
-      response.send('404 Not Found....');
+
+      response.render(`${__dirname}/../../assets/views/http`, {
+        status: 404,
+        message: 'Not Found',
+      });
     });
 
     app.listen(port, () => {
