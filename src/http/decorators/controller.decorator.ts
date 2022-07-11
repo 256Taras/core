37ee4e6a03e1b8c70e3c223f8cf97cba710d7b3c
Doc077 <dom.rajkowski@gmail.com>
2022-07-11 19:58:37 +0200

    Fix type

--- src/http/decorators/controller.decorator.ts
+++ src/http/decorators/controller.decorator.ts
@@ -2,7 +2,7 @@ import { ClassDecorator } from '../../utils/types/class-decorator.type';
 import { Constructor } from '../../utils/interfaces/constructor.interface';
 
 export const Controller = (baseUrl?: string): ClassDecorator<Constructor> => {
-  return (target: Constructor) => {
+  return (target: any) => {
     return class extends target {
       public baseUrl = baseUrl;
     };
