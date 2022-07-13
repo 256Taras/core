51bec0754e33ebcf0f410aac9a61f612f0334527
Doc077 <dom.rajkowski@gmail.com>
2022-07-13 21:31:59 +0200

    Add HTTP status page rendering on production

--- src/handler/handler.class.ts
+++ src/handler/handler.class.ts
@@ -1,3 +1,4 @@
+import { env } from '../config/env.function';
 import { Exception } from './exception.class';
 import { Request, Response, NextFunction } from 'express';
 
@@ -10,13 +11,19 @@ export class Handler {
   ): void {
     response.status(500);
 
+    const data = {
+      status: 500,
+      message: 'Server Error',
+    };
+
     if (request.xhr || request.headers.accept?.includes('json')) {
-      response.send({
-        status: 500,
-        message: 'Server Error',
-      });
+      response.send(data);
 
       return;
+    }
+
+    if (!env<boolean>('APP_DEBUG')) {
+      response.render(`${__dirname}/../../assets/views/http`, data);
     }
 
     response.render(`${__dirname}/../../assets/views/exception`, {
