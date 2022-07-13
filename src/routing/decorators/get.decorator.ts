a70c40f90b7656b4006c4d1c1208f19ec33de3d0
Doc077 <dom.rajkowski@gmail.com>
2022-07-13 10:07:59 +0200

    Format

--- src/routing/decorators/get.decorator.ts
+++ src/routing/decorators/get.decorator.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { Router } from '../router.class';
 
 namespace Route {
-  export const Get = (url: string): (target: any, controllerMethod: string) => void => {
+  export const Get = (
+    url: string,
+  ): (target: any, controllerMethod: string) => void => {
     return (target: any, controllerMethod: string) => {
       Router.get(url, async (_request: Request, response: Response) => {
-        response.send(Router.resolveController(target.constructor, controllerMethod));
+        response.send(
+          Router.resolveController(target.constructor, controllerMethod),
+        );
       });
     };
   };
--- src/routing/decorators/post.decorator.ts
+++ src/routing/decorators/post.decorator.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { Router } from '../router.class';
 
 namespace Route {
-  export const Post = (url: string): (target: any, controllerMethod: string) => void => {
+  export const Post = (
+    url: string,
+  ): (target: any, controllerMethod: string) => void => {
     return (target: any, controllerMethod: string) => {
       Router.post(url, async (_request: Request, response: Response) => {
-        response.send(Router.resolveController(target.constructor, controllerMethod));
+        response.send(
+          Router.resolveController(target.constructor, controllerMethod),
+        );
       });
     };
   };
