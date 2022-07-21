3e172f8482c3fcbfbc23159206c0398f972bb5c0
Doc077 <dom.rajkowski@gmail.com>
2022-07-21 22:26:46 +0200

    Fix args

--- src/http/view-response.class.ts
+++ src/http/view-response.class.ts
@@ -1,3 +1,3 @@
 export class ViewResponse {
-  constructor(public file: string, public data: Record<string, any>) {}
+  constructor(public file: string, public data?: Record<string, any>) {}
 }
--- src/views/functions/render.function.ts
+++ src/views/functions/render.function.ts
@@ -1,5 +1,5 @@
 import { ViewResponse } from '../../http/view-response.class';
 
-export const render = (file: string, data: Record<string, any>): ViewResponse => {
+export const render = (file: string, data?: Record<string, any>): ViewResponse => {
   return new ViewResponse(file, data);
 }
