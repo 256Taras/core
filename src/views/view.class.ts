ea29863a5fe21077712ed7d152ddf3eba794d639
Doc077 <dom.rajkowski@gmail.com>
2022-07-25 14:52:05 +0200

    Improve view data rendering

--- src/views/view.class.ts
+++ src/views/view.class.ts
@@ -1,42 +1,33 @@
 import * as constants from '../constants';
-import { Exception } from '../handler/exception.class';
 import { encode } from 'html-entities';
 import { readFileSync } from 'node:fs';
 
 export class View {
   public static parse(
     filePath: string,
-    variables: Record<string, any>,
+    data: Record<string, any>,
     callback: (e: any, rendered?: string | undefined) => void,
   ) {
     let compiled = readFileSync(filePath).toString();
 
-    for (const expression of compiled.matchAll(/\{(\*?)([a-zA-Z0-9]*?)\}/g) ?? []) {
-      const name: string = expression[2];
-      const isConstant = name.startsWith('NUCLEON_') || name.startsWith('NODE_');
+    for (const expression of compiled.matchAll(/\{(\*?)(.*?)\}/g) ?? []) {
+      const value: string = expression[2];
 
-      let variableValue: string = isConstant
-        ? constants[name as keyof object]
-        : variables[name];
+      const scopeVariables = {
+        ...constants,
+        ...data,
+      };
 
-      if (isConstant && !(name in constants)) {
-        throw new Exception(`The '${name}' constant is not defined`);
-      }
+      const functionHeaderData = [
+        ...Object.keys(scopeVariables),
+        `return ${value}`,
+      ];
 
-      if (!isConstant && !(name in variables)) {
-        throw new Exception(
-          `The '${name}' variable has not been passed to the view`,
-        );
-      }
+      const fn = new Function(...functionHeaderData);
 
-      const plainValue = expression[1] ? variableValue : encode(variableValue);
+      const returnedValue = fn(...Object.values(scopeVariables));
 
-      variableValue =
-        Array.isArray(variableValue) || typeof variableValue === 'object'
-          ? JSON.stringify(variableValue)
-          : plainValue;
-
-      compiled = compiled.replace(expression[0], variableValue);
+      compiled = compiled.replace(expression[0], returnedValue);
     }
 
     return callback(null, compiled);
