e3ad762c3911cd860a692a5be5a319c661d8f693
Doc077 <dom.rajkowski@gmail.com>
2022-08-01 13:29:16 +0200

    Add relative view paths support

--- src/views/functions/render.function.ts
+++ src/views/functions/render.function.ts
@@ -1,11 +1,12 @@
 import { ViewResponse } from '../../http/view-response.class';
+import { fileURLToPath } from 'node:url';
 
 function getFileCallerURL(): string {
   const error = new Error();
   const stack = error.stack?.split('\n') as string[];
-  const data: string = stack[3];
+  const data = stack[3];
 
-  const filePathPattern = new RegExp(`(file:[/]{2}.+[^:0-9]):{1}[0-9]+:{1}[0-9]+`);
+  const filePathPattern = /(file:[/]{2}.+[^:0-9]):{1}[0-9]+:{1}[0-9]+/;
   const result = filePathPattern.exec(data);
 
   let filePath: string = '';
@@ -14,12 +15,16 @@ function getFileCallerURL(): string {
     filePath = result[1];
   }
 
-  return filePath;
+  return fileURLToPath(filePath);
 } 
 
 export const render = (file: string, data?: Record<string, any>): ViewResponse => {
   const callerFile = getFileCallerURL();
-  console.log(callerFile);
+
+  if (file.startsWith('./') || file.startsWith('../')) {
+    file = `${callerFile}/${file};`
+  }
+  console.log(file);
 
   return new ViewResponse(file, data);
 };
