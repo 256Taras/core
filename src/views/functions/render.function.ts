4369bea01b95a8c669f9eebb5b4283b59c049c1e
Doc077 <dom.rajkowski@gmail.com>
2022-08-02 21:08:12 +0200

    Refactor

--- src/views/functions/render.function.ts
+++ src/views/functions/render.function.ts
@@ -1,25 +1,8 @@
 import { ViewResponse } from '../../http/view-response.class';
-import { fileURLToPath } from 'node:url';
-
-function getFileCallerURL(): string {
-  const error = new Error();
-  const stack = error.stack?.split('\n') as string[];
-  const data = stack[3];
-
-  const filePathPattern = /(file:[/]{2}.+[^:0-9]):{1}[0-9]+:{1}[0-9]+/;
-  const result = filePathPattern.exec(data);
-
-  let filePath: string = '';
-
-  if (result && result.length > 1) {
-    filePath = result[1];
-  }
-
-  return fileURLToPath(filePath);
-}
+import { getCallerFile } from '../../utils/functions/get-caller-file.function';
 
 export const render = (file: string, data?: Record<string, any>): ViewResponse => {
-  const callerFile = getFileCallerURL();
+  const callerFile = getCallerFile();
 
   if (file.startsWith('./')) {
     file = `${callerFile}/../${file.slice(2)}`;
