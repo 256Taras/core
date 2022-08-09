7963e16cd0c1ea075857050a92aa6bad7d01753a
Doc077 <dom.rajkowski@gmail.com>
2022-08-09 12:11:56 +0200

    Fix injector caching

--- src/injector/injector.class.ts
+++ src/injector/injector.class.ts
@@ -1,14 +1,14 @@
 import { Constructor } from '../utils/interfaces/constructor.interface';
 
 export class Injector {
-  private static singletons: Map<Constructor, any> = new Map();
+  private static cachedInstances: Map<Constructor, any> = new Map();
 
   public static bind(targets: Constructor | Constructor[]): void {
     if (Array.isArray(targets)) {
       targets.map((target) => {
         const instance = this.resolve<Constructor>(target);
 
-        this.singletons.set(target, instance);
+        this.cachedInstances.set(target, instance);
       });
 
       return;
@@ -16,22 +16,28 @@ export class Injector {
 
     const instance = this.resolve<Constructor>(targets);
 
-    this.singletons.set(targets, instance);
+    this.cachedInstances.set(targets, instance);
   }
 
   public static get<T = any>(className: Constructor<T>): T {
-    return this.singletons.get(className);
+    return this.cachedInstances.get(className);
   }
 
   public static has(className: Constructor): boolean {
-    return this.singletons.has(className);
+    return this.cachedInstances.has(className);
   }
 
   public static resolve<T>(target: Constructor<T>): T {
     const deps = Reflect.getMetadata('design:paramtypes', target) ?? [];
     const resolved = deps.map((param: Constructor) => this.resolve(param));
 
-    const instance = this.has(target) ? this.get(target) : new target(...resolved);
+    if (this.has(target)) {
+      return this.get(target);
+    }
+
+    const instance = new target(...resolved);
+
+    this.cachedInstances.set(target, instance);
 
     return instance;
   }
