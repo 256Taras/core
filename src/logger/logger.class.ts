c8a220d79745281c7ff193c98e1899b88ec79068
Doc077 <dom.rajkowski@gmail.com>
2022-08-09 18:43:51 +0200

    Refactor logger

--- src/logger/logger.class.ts
+++ src/logger/logger.class.ts
@@ -3,33 +3,56 @@ import chalk from 'chalk';
 
 @Service()
 export class Logger {
-  public error(data: string, type: string = 'error'): void {
+  private readonly colorYellow = '#f8c377';
+
+  private readonly mark = '$';
+
+  private getDay(): string {
     const date = new Date();
 
-    const locale = 'en-us';
-
-    const day = date.toLocaleString(locale, {
+    return date.toLocaleString('en-us', {
       month: 'short',
       day: 'numeric',
     });
+  }
 
-    const time = date.toLocaleString(locale, {
+  private getTime(): string {
+    const date = new Date();
+
+    return date.toLocaleString('en-us', {
       hour: 'numeric',
       minute: 'numeric',
       second: '2-digit',
       hour12: true,
     });
+  }
 
-    const timestamp = `${chalk.red.bold('$')} ${chalk.gray(day)} ${chalk.gray(time)} `;
+  private renderDots(data: string): string {
+    const outputLength = data.length + 18;
+    const dots = chalk.gray('.'.repeat(process.stdout.columns - outputLength));
 
+    return dots;
+  }
+
+  private truncate(data: string): string {
     const maxLength = Math.trunc(process.stdout.columns / 2);
-    const main = data.length > maxLength ? data.slice(0, maxLength) + '...' : data;
-    const outputLength = (day + time + main + type).length + 18;
+    const truncated = data.length > maxLength ? data.slice(0, maxLength) + '...' : data;
+
+    return truncated;
+  }
+
+  public error(data: string, type: string = 'error'): void {
+    const day = this.getDay();
+    const time = this.getTime();
+
+    const timestamp = `${chalk.red.bold(this.mark)} ${chalk.gray(day)} ${chalk.gray(time)} `;
+
+    const main = this.truncate(data);
 
     const left = `${timestamp} ${chalk.red.bold(main)}`;
     const right = chalk.gray(type.toUpperCase());
 
-    const dots = chalk.gray('.'.repeat(process.stdout.columns - outputLength));
+    const dots = this.renderDots(day + time + main + type);
 
     console.error(left, dots, right);
   }
@@ -41,69 +64,37 @@ export class Logger {
   }
 
   public log(data: string, type: string = 'info'): void {
-    const date = new Date();
+    const day = this.getDay();
+    const time = this.getTime();
 
-    const locale = 'en-us';
-
-    const day = date.toLocaleString(locale, {
-      month: 'short',
-      day: 'numeric',
-    });
-
-    const time = date.toLocaleString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: '2-digit',
-      hour12: true,
-    });
-
-    const timestamp = `${chalk.green.bold('$')} ${chalk.gray(day)} ${chalk.gray(
+    const timestamp = `${chalk.green.bold(this.mark)} ${chalk.gray(day)} ${chalk.gray(
       time,
     )} `;
 
-    const maxLength = Math.trunc(process.stdout.columns / 2);
-    const main = data.length > maxLength ? data.slice(0, maxLength) + '...' : data;
-    const outputLength = (day + time + main + type).length + 18;
+    const main = this.truncate(data);
 
     const left = `${timestamp} ${chalk.white.bold(main)}`;
     const right = chalk.gray(type.toUpperCase());
 
-    const dots = chalk.gray('.'.repeat(process.stdout.columns - outputLength));
+    const dots = this.renderDots(day + time + main + type);
 
     console.log(left, dots, right);
   }
 
   public warn(data: string, type: string = 'warning'): void {
-    const date = new Date();
+    const day = this.getDay();
+    const time = this.getTime();
 
-    const locale = 'en-us';
-
-    const day = date.toLocaleString(locale, {
-      month: 'short',
-      day: 'numeric',
-    });
-
-    const time = date.toLocaleString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: '2-digit',
-      hour12: true,
-    });
-
-    const yellowHex = '#f8c377';
-
-    const timestamp = `${chalk.hex(yellowHex).bold('$')} ${chalk.gray(
+    const timestamp = `${chalk.hex(this.colorYellow).bold(this.mark)} ${chalk.gray(
       day,
     )} ${chalk.gray(time)} `;
 
-    const maxLength = Math.trunc(process.stdout.columns / 2);
-    const main = data.length > maxLength ? data.slice(0, maxLength) + '...' : data;
-    const outputLength = (day + time + main + type).length + 18;
+    const main = this.truncate(data);
 
-    const left = `${timestamp} ${chalk.hex(yellowHex).bold(main)}`;
+    const left = `${timestamp} ${chalk.hex(this.colorYellow).bold(main)}`;
     const right = chalk.gray(type.toUpperCase());
 
-    const dots = chalk.gray('.'.repeat(process.stdout.columns - outputLength));
+    const dots = this.renderDots(day + time + main + type);
 
     console.warn(left, dots, right);
   }
