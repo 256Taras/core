588cc9fed8768d877b665dca0529d0eeaf1263a0
Doc077 <dom.rajkowski@gmail.com>
2022-09-02 19:30:40 +0200

    Fix test

--- tests/utils/run-command.test.ts
+++ tests/utils/run-command.test.ts
@@ -2,9 +2,11 @@ import { describe, expect, it } from 'vitest';
 import { runCommand } from '../../src/utils/functions/run-command.function';
 
 describe('runCommand function', () => {
-  it('returns correctly debounced callback', () => {
+  it('runs system commands', () => {
     const success = runCommand('npm -v');
+    const failure = runCommand('non-existing-command');
 
     expect(success).toBe(true);
+    expect(failure).toBe(false);
   });
 });
