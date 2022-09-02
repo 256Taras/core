9df27489d3f96f7b6cf8fa4146996c3dd996776e
Doc077 <dom.rajkowski@gmail.com>
2022-09-02 18:48:08 +0200

    Add encrypter test

---
+++ tests/crypto/encrypter.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest';
+import { inject } from '../../src/injector/functions/inject.function';
+import { Encrypter } from '../../src/crypto/encrypter.class';
+
+describe('Encrypter class', () => {
+  const encrypter = inject(Encrypter);
+
+  const data = 'test';
+
+  it('correctly encrypts data', async () => {
+    const encryptedData = await encrypter.encrypt(data);
+    const decryptedData = await encrypter.decrypt(encryptedData);
+
+    expect(decryptedData).toBe(data);
+  });
+
+  it('correctly hashes data', async () => {
+    const hash = await encrypter.hash(data);
+    const equal = await encrypter.compareHash(data, hash);
+
+    expect(equal).toBe(true);
+  });
+
+  it('generates random UUID', async () => {
+    const uuid = encrypter.uuid();
+
+    expect(typeof uuid).toBe('string');
+  });
+});
