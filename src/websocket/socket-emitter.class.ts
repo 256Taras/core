4dd3dbdc8b04c581c325bba17d1b340e052de002
Doc077 <dom.rajkowski@gmail.com>
2022-09-08 21:09:35 +0200

    Improve socket emitter

--- src/websocket/socket-emitter.class.ts
+++ src/websocket/socket-emitter.class.ts
@@ -3,6 +3,8 @@ import { Server } from '../server/server.class';
 import { Service } from '../injector/decorators/service.decorator';
 import { Logger } from '../logger/logger.class';
 import { Authorizer } from './interfaces/authorizer.nterface';
+import { inject } from '../injector/functions/inject.function';
+import { Constructor } from '../utils/interfaces/constructor.interface';
 
 @Service()
 export class SocketEmitter {
@@ -14,19 +16,27 @@ export class SocketEmitter {
     this.socketServer = new SocketServer(this.server.$nativeHttpServer());
 
     this.socketServer.on('connection', (socket: Socket) => {
-      this.logger.log(`Established connection: ${socket.id}`, 'socket')
-    })
+      this.logger.log(`Established connection: ${socket.id}`, 'socket');
+    });
   }
 
   public emit(event: string, channelName: string, ...data: unknown[]): void {
     this.channels.forEach((channel) => {
       if (channel.namePattern.test(channelName) && channel.passesUser()) {
-        this.socketServer?.emit(`${channelName}/${event}`, ...data);
+        this.socketServer.emit(`${channelName}/${event}`, ...data);
 
         this.logger.log(`Emitted: ${channelName}/${event}`, 'socket');
 
         return;
       }
+    })
+  }
+
+  public registerChannels(channels: Authorizer[]): void {
+    channels.forEach((channel) => {
+      const instance = inject(channel as unknown as Constructor);
+
+      this.channels.push(instance);
     })
   }
 }
