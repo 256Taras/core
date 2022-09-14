7a683dca72ba5d908dd32ea8a03951d66323f9b8
Doc077 <dom.rajkowski@gmail.com>
2022-09-14 16:12:53 +0200

    Add env() default value support

--- src/utils/functions/env.function.ts
+++ src/utils/functions/env.function.ts
@@ -1,13 +1,13 @@
-export const env = <T = any>(key: string): T => {
+export const env = <T = any>(key: string, defaultValue: unknown = null): T => {
   if (!(key in process.env)) {
     return null as unknown as T;
   }
 
   try {
-    const casted = JSON.parse(process.env[key]?.toString() ?? 'null');
+    const casted = JSON.parse(process.env[key]?.toString() ?? JSON.parse(defaultValue));
 
     return casted;
   } catch {
-    return (process.env[key] ?? null) as unknown as T;
+    return (process.env[key] ?? defaultValue) as unknown as T;
   }
 };
