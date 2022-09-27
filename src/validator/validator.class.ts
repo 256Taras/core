0996c8261ef32a34278399e87c7ecaf66cd4d693
Doc077 <dom.rajkowski@gmail.com>
2022-09-27 19:27:19 +0200

    Add validator messages

--- src/validator/validator.class.ts
+++ src/validator/validator.class.ts
@@ -14,196 +14,196 @@ export class Validator {
 
   constructor(private request: Request, private response: Response) {}
 
-  private validateAccepted(value: string, isAccepted: boolean): boolean {
+  private validateAccepted(value: string, isAccepted: boolean, fieldName: string): boolean | string {
     if (isAccepted && !value) {
-      return false;
+      return `Field ${fieldName} must be accepted`;
     }
 
     return true;
   }
 
-  private validateDate(value: string, isDate: boolean): boolean {
+  private validateDate(value: string, isDate: boolean, fieldName: string): boolean | string {
     if (
       (isDate && (new Date(value) as unknown) === 'Invalid Date') ||
       isNaN(new Date(value) as unknown as number)
     ) {
-      return false;
+      return `Field ${fieldName} must be a valid date format`;
     }
 
     return true;
   }
 
-  private validateDoesntEndWith(value: string, search: string): boolean {
+  private validateDoesntEndWith(value: string, search: string, fieldName: string): boolean | string {
     if (value.endsWith(search)) {
-      return false;
+      return `Field ${fieldName} must not end with '${search}'`;
     }
 
     return true;
   }
 
-  private validateDoesntStartWith(value: string, search: string): boolean {
+  private validateDoesntStartWith(value: string, search: string, fieldName: string): boolean | string {
     if (value.endsWith(search)) {
-      return false;
+      return `Field ${fieldName} must not start with '${search}'`;
     }
 
     return true;
   }
 
-  private validateEndsWith(value: string, search: string): boolean {
+  private validateEndsWith(value: string, search: string, fieldName: string): boolean | string {
     if (!value.endsWith(search)) {
-      return false;
+      return `Field ${fieldName} must end with '${search}'`;
     }
 
     return true;
   }
 
-  private validateEmail(value: string, isEmail: boolean): boolean {
+  private validateEmail(value: string, isEmail: boolean, fieldName: string): boolean | string {
     if (isEmail && !this.emailRegexp.test(value)) {
-      return false;
+      return `Field ${fieldName} must be a valid email`;
     }
 
     return true;
   }
 
-  private validateFloat(value: number, isFloat: boolean): boolean {
+  private validateFloat(value: number, isFloat: boolean, fieldName: string): boolean | string {
     if ((isFloat && Number.isInteger(value)) || isNaN(value)) {
-      return false;
+      return `Field ${fieldName} must be a floating point number`;
     }
 
     return true;
   }
 
-  private validateIn(value: string, array: any[]): boolean {
+  private validateIn(value: string, array: any[], fieldName: string): boolean | string {
     if (!array.includes(value)) {
-      return false;
+      return `Field ${fieldName} must be a value from [${array.join(', ')}]`;
     }
 
     return true;
   }
 
-  private validateInteger(value: number, isInteger: boolean): boolean {
+  private validateInteger(value: number, isInteger: boolean, fieldName: string): boolean | string {
     if ((isInteger && !Number.isInteger(value)) || isNaN(value)) {
-      return false;
+      return `Field ${fieldName} must be an integer number`;
     }
 
     return true;
   }
 
-  private validateIp(value: string, ip: boolean): boolean {
+  private validateIp(value: string, ip: boolean, fieldName: string): boolean | string {
     if (ip && !isIP(value)) {
-      return false;
+      return `Field ${fieldName} must be a valid IP address`;
     }
 
     return true;
   }
 
-  private validateIpv4(value: string, ipv4: boolean): boolean {
+  private validateIpv4(value: string, ipv4: boolean, fieldName: string): boolean | string {
     if (ipv4 && !isIPv4(value)) {
-      return false;
+      return `Field ${fieldName} must be a valid IPv4 address`;
     }
 
     return true;
   }
 
-  private validateLength(value: string, length: number): boolean {
+  private validateLength(value: string, length: number, fieldName: string): boolean | string {
     if (value.length !== length) {
-      return false;
+      return `Field ${fieldName} must be ${length} characters long`;
     }
 
     return true;
   }
 
-  private validateMax(value: number, length: number): boolean {
+  private validateMax(value: number, length: number, fieldName: string): boolean | string {
     if (isNaN(value) || Number(value) > length) {
-      return false;
+      return `Field ${fieldName} must be less than ${length}`;
     }
 
     return true;
   }
 
-  private validateMaxLength(value: string, length: number): boolean {
+  private validateMaxLength(value: string, length: number, fieldName: string): boolean | string {
     if (value.length > length) {
-      return false;
+      return `Field ${fieldName} must be shorther than ${length} characters`;
     }
 
     return true;
   }
 
-  private validateMin(value: number, length: number): boolean {
+  private validateMin(value: number, length: number, fieldName: string): boolean | string {
     if (isNaN(value) || Number(value) < length) {
-      return false;
+      return `Field ${fieldName} must be greater than ${length}`;
     }
 
     return true;
   }
 
-  private validateMinLength(value: string, length: number): boolean {
+  private validateMinLength(value: string, length: number, fieldName: string): boolean | string {
     if (value.length < length) {
-      return false;
+      return `Field ${fieldName} must be longer than ${length} characters`;
     }
 
     return true;
   }
 
-  private validateNotIn(value: string, array: any[]): boolean {
+  private validateNotIn(value: string, array: any[], fieldName: string): boolean | string {
     if (array.includes(value)) {
-      return false;
+      return `Field ${fieldName} must not be a value from [${array.join(', ')}]`;
     }
 
     return true;
   }
 
-  private validateNumeric(value: number, isNumeric: boolean): boolean {
+  private validateNumeric(value: number, isNumeric: boolean, fieldName: string): boolean | string {
     if (isNumeric && isNaN(value)) {
-      return false;
+      return `Field ${fieldName} must be numeric`;
     }
 
     return true;
   }
 
-  private validateOtherThan(value: string, search: string): boolean {
+  private validateOtherThan(value: string, search: string, fieldName: string): boolean | string {
     if (value === search) {
-      return false;
+      return `Field ${fieldName} must be other than '${search}'`;
     }
 
     return true;
   }
 
-  private validateRegexp(value: string, regexp: RegExp): boolean {
+  private validateRegexp(value: string, regexp: RegExp, fieldName: string): boolean | string {
     if (!regexp.test(value)) {
-      return false;
+      return `Field ${fieldName} must follow the ${regexp.toString()} pattern`;
     }
 
     return true;
   }
 
-  private validateRequired(value: string, isRequired: boolean): boolean {
+  private validateRequired(value: string, isRequired: boolean, fieldName: string): boolean | string {
     if ((isRequired && !value) || value === '') {
-      return false;
+      return `Field ${fieldName} is required`;
     }
 
     return true;
   }
 
-  private validateSameAs(value: string, secondField: string): boolean {
+  private validateSameAs(value: string, secondField: string, fieldName: string): boolean | string {
     if (value !== this.request.input(secondField)) {
-      return false;
+      return `Field ${fieldName} must be same as ${secondField}`;
     }
 
     return true;
   }
 
-  private validateStartsWith(value: string, search: string): boolean {
+  private validateStartsWith(value: string, search: string, fieldName: string): boolean | string {
     if (!value.startsWith(search)) {
-      return false;
+      return `Field ${fieldName} must start with '${search}'`;
     }
 
     return true;
   }
 
-  private validateUsername(value: string, isUsername: boolean): boolean {
+  private validateUsername(value: string, isUsername: boolean, fieldName: string): boolean | string {
     if (isUsername && !this.usernameRegexp.test(value)) {
-      return false;
+      return `Field ${fieldName} must be a valid username`;
     }
 
     return true;
@@ -237,17 +237,21 @@ export class Validator {
       username: this.validateUsername,
     };
 
-    for (const [field, ruleSet] of Object.entries(rules)) {
-      const fieldValue = this.request.input(field);
+    for (const [fieldName, ruleSet] of Object.entries(rules)) {
+      const fieldValue = this.request.input(fieldName);
 
       for (const [rule, ruleValue] of Object.entries(ruleSet)) {
         if (!(rule in ruleMapper)) {
           throw new Error(`Invalid validation rule '${rule}'`);
         }
 
-        if (!ruleMapper[rule](fieldValue, ruleValue)) {
+        const messagesOrSuccess = ruleMapper[rule](fieldValue, ruleValue, fieldName);
+
+        if (typeof messagesOrSuccess === 'string') {
           if (!checkOnly) {
-            this.response.redirectBack({}, StatusCode.BadRequest);
+            this.response.redirectBack({
+              errors: messagesOrSuccess,
+            }, StatusCode.BadRequest);
 
             continue;
           }
