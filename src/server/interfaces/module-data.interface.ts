4d602d8555766d01ba7804fa9968588411bc1093
Doc077 <dom.rajkowski@gmail.com>
2022-10-15 19:32:19 +0200

    Add imports field to module interface

--- src/server/interfaces/module-data.interface.ts
+++ src/server/interfaces/module-data.interface.ts
@@ -1,6 +1,7 @@
 import { Constructor } from '../../utils/interfaces/constructor.interface';
 
 export interface ModuleData {
+  imports?: Constructor[];
   controllers?: Constructor[];
   socketChannels?: Constructor[];
 }
