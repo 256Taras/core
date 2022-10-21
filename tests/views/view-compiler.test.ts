94a554c79fbde4f20e549448306ee25b8f2b9b1a
Doc077 <dom.rajkowski@gmail.com>
2022-10-21 20:32:13 +0200

    Improve view compiler tests

--- tests/views/view-compiler.test.ts
+++ tests/views/view-compiler.test.ts
@@ -5,16 +5,47 @@ import { ViewCompiler } from '../../src/views/view-compiler.class';
 describe('ViewCompiler class', () => {
   const compiler = inject(ViewCompiler);
 
-  it('correctly compiles template', async () => {
-    const template = '[if condition]{message}[/if]';
+  it('correctly compiles data renders', async () => {
+    const template = '{{ count + 1 }}';
 
     const data = {
-      message: 'Test',
-      condition: true,
+      count: 2,
     };
 
     const compiled = await compiler.compile(template, data);
 
-    expect(compiled).toBe(data.message);
+    expect(compiled).toBe('3');
+  });
+
+  it('correctly compiles [each] blocks', async () => {
+    const template = '[each (item in 3)]{{ item }}[/each]';
+
+    const compiled = await compiler.compile(template);
+
+    expect(compiled).toBe('0123');
+  });
+
+  it('correctly compiles [if] blocks', async () => {
+    const template = '[if (true)]northle[/if]';
+
+    const compiled = await compiler.compile(template);
+
+    expect(compiled).toBe('northle');
+  });
+
+  it('correctly compiles [method] blocks', async () => {
+    const template = `[method('POST')]`;
+
+    const compiled = await compiler.compile(template);
+
+    expect(compiled.includes('<input')).toBe(true);
+  });
+
+  it('correctly compiles [token] blocks', async () => {
+    const template = `[token]`;
+
+    const compiled = await compiler.compile(template);
+
+    expect(compiled.includes('<input')).toBe(true);
   });
 });
