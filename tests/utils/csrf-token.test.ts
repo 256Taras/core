f2227697c3c82d49b1d46fce926323c3b08940ed
Doc077 <dom.rajkowski@gmail.com>
2022-10-21 21:06:08 +0200

    Make tests pass

--- tests/utils/csrf-token.test.ts
+++ tests/utils/csrf-token.test.ts
@@ -1,7 +1,39 @@
 import { describe, expect, it } from 'vitest';
+import fastify from 'fastify';
+import csrfMiddleware from '@fastify/csrf-protection';
+import sessionMiddleware from '@fastify/session';
+import cookieMiddleware from '@fastify/cookie';
 import { csrfToken } from '../../src/utils/functions/csrf-token.function';
+import { inject } from '../../src/injector/functions/inject.function';
+import { Request } from '../../src/http/request.class';
+import { Response } from '../../src/http/response.class';
+import { Encrypter } from '../../src/crypto/encrypter.class';
 
-describe('csrfToken function', () => {
+describe('csrfToken function', async () => {
+  const encrypter = inject(Encrypter);
+
+  const app = fastify();
+
+  await app.register(cookieMiddleware);
+
+  await app.register(sessionMiddleware, {
+    secret: encrypter.randomBytes(16),
+  });
+
+  await app.register(csrfMiddleware);
+
+  app.get('/', async (request, response) => {
+    inject(Request).$setInstance(request);
+    inject(Response).$setInstance(response);
+
+    response.send('Northle');
+  });
+
+  await app.inject({
+    method: 'GET',
+    url: '/'
+  });
+
   it('returns CSRF token', async () => {
     const token = await csrfToken();
 
--- tests/validator/validator.test.ts
+++ tests/validator/validator.test.ts
@@ -1,11 +1,42 @@
 import { describe, expect, it } from 'vitest';
+import fastify from 'fastify';
+import csrfMiddleware from '@fastify/csrf-protection';
+import sessionMiddleware from '@fastify/session';
+import cookieMiddleware from '@fastify/cookie';
 import { inject } from '../../src/injector/functions/inject.function';
+import { Request } from '../../src/http/request.class';
+import { Response } from '../../src/http/response.class';
+import { Encrypter } from '../../src/crypto/encrypter.class';
 import { Validator } from '../../src/validator/validator.class';
 
-describe('Validator class', () => {
-  const validator = inject(Validator);
+describe('Validator class', async () => {
+  const encrypter = inject(Encrypter);
+
+  const app = fastify();
+
+  await app.register(cookieMiddleware);
+
+  await app.register(sessionMiddleware, {
+    secret: encrypter.randomBytes(16),
+  });
+
+  await app.register(csrfMiddleware);
+
+  app.get('/', async (request, response) => {
+    inject(Request).$setInstance(request);
+    inject(Response).$setInstance(response);
+
+    response.send('Northle');
+  });
 
   it('asserts data is valid', async () => {
+    await app.inject({
+      method: 'GET',
+      url: '/'
+    });
+
+    const validator = inject(Validator);
+
     const isValid = validator.assert({
       name: {
         required: true,
