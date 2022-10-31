de1b3085c41007d78c2085ceb1cb8e0f0ad0b6b8
Doc077 <dom.rajkowski@gmail.com>
2022-10-31 20:12:25 +0100

    Add support for mail views

--- src/mailer/interfaces/mail-data.interface.ts
+++ src/mailer/interfaces/mail-data.interface.ts
@@ -2,5 +2,6 @@ export interface MailData {
   to: string;
   subject: string;
   text?: string;
-  html?: string;
+  view?: string;
+  viewData?: Record<string, unknown>;
 }
--- src/mailer/mailer.class.ts
+++ src/mailer/mailer.class.ts
@@ -1,13 +1,16 @@
 import { Transporter, createTransport } from 'nodemailer';
+import { readFile } from 'node:fs/promises';
 import { Service } from '../injector/decorators/service.decorator';
 import { env } from '../utils/functions/env.function';
 import { MailData } from './interfaces/mail-data.interface';
+import { ViewCompiler } from '../views/view-compiler.class';
+import { callerFile } from '../utils/functions/caller-file.function';
 
 @Service()
 export class Mailer {
   private transporter: Transporter;
 
-  constructor() {
+  constructor(private viewCompiler: ViewCompiler) {
     try {
       this.transporter = createTransport({
         host: env('MAIL_HOST') ?? 'smtp.gmail.com',
@@ -19,19 +22,34 @@ export class Mailer {
         },
       });
     } catch (error) {
-      throw new Error('Failed to setup mail service');
+      throw new Error('Failed to set up mail service');
     }
   }
 
   public async send(data: MailData): Promise<string> {
-    const { to, subject, text, html } = data;
+    const { to, subject, text, viewData } = data;
+
+    let html = '';
+    let { view } = data;
+
+    if (view) {
+      const caller = callerFile();
+
+      if (view.startsWith('./')) {
+        view = `${caller}/../${view.slice(2)}`;
+      }
+
+      const fileContent = await readFile(view, 'utf-8');
+
+      html = await this.viewCompiler.compile(fileContent, viewData);
+    }
 
     const info = await this.transporter.sendMail({
       from: env('MAIL_ADDRESS') ?? '',
       to,
       subject,
-      text,
-      ...(html && { html }),
+      ...(text && { text }),
+      ...(view && { html }),
     });
 
     return info.messageId;
