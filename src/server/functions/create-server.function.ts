95822f14fdb4bad2eea846bf7744d716b5a82195
Doc077 <dom.rajkowski@gmail.com>
2022-11-02 17:03:50 +0100

    Rewrite createServer()

--- src/server/functions/create-server.function.ts
+++ src/server/functions/create-server.function.ts
@@ -1,9 +1,9 @@
-import { Injector } from '../../injector/injector.class';
+import { inject } from '../../injector/functions/inject.function';
 import { ServerOptions } from '../interfaces/server-options.interface';
 import { Server } from '../server.class';
 
 export const createServer = async (options: ServerOptions) => {
-  const server = Injector.resolve(Server);
+  const server = await (inject(Server).setup(options));
 
-  return await server.setup(options);
+  return server;
 };
