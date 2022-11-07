d90633e07b4e61719d7cf9dd9a5803b491a8f87e
Doc077 <dom.rajkowski@gmail.com>
2022-11-07 21:13:04 +0100

    Fix csrfToken test

--- tests/utils/csrf-token.test.ts
+++ tests/utils/csrf-token.test.ts
@@ -1,9 +1,5 @@
-import cookieMiddleware from '@fastify/cookie';
-import csrfMiddleware from '@fastify/csrf-protection';
-import sessionMiddleware from '@fastify/session';
 import fastify from 'fastify';
 import { describe, expect, it } from 'vitest';
-import { Encrypter } from '../../src/crypto/encrypter.class';
 import { Request } from '../../src/http/request.class';
 import { Response } from '../../src/http/response.class';
 import { inject } from '../../src/injector/functions/inject.function';
@@ -11,24 +7,14 @@ import { Session } from '../../src/session/session.class';
 import { csrfToken } from '../../src/utils/functions/csrf-token.function';
 
 describe('csrfToken function', async () => {
-  const encrypter = inject(Encrypter);
-  const request = inject(Request);
-
-  inject(Session).$setRequest(request);
-
   const app = fastify();
-
-  await app.register(cookieMiddleware);
-
-  await app.register(sessionMiddleware, {
-    secret: encrypter.randomBytes(16),
-  });
-
-  await app.register(csrfMiddleware);
 
   app.get('/', async (request, response) => {
     inject(Request).$setInstance(request);
     inject(Response).$setInstance(response);
+
+    inject(Session).$setRequest(request);
+    inject(Session).$setResponse(response);
 
     response.send('Northle');
   });
@@ -38,8 +24,8 @@ describe('csrfToken function', async () => {
     url: '/',
   });
 
-  it('returns CSRF token', async () => {
-    const token = await csrfToken();
+  it('returns CSRF token', () => {
+    const token = csrfToken();
 
     expect(typeof token).toBe('string');
   });
