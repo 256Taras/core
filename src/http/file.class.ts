0c875b3d38627c66c00a34af2bb7f3918d36fee3
Doc077 <dom.rajkowski@gmail.com>
2022-11-11 21:35:45 +0100

    Remove unnecessary temp file deleting

--- src/http/file.class.ts
+++ src/http/file.class.ts
@@ -1,4 +1,4 @@
-import { copyFile, rm } from 'node:fs/promises';
+import { copyFile } from 'node:fs/promises';
 
 export class File {
   constructor(public name: string, public path: string) {}
@@ -6,8 +6,6 @@ export class File {
   public async store(path: string, name: string): Promise<void> {
     try {
       await copyFile(this.path, `${path}/${name ?? this.name}`);
-
-      await rm(this.path);
     } catch (error) {
       throw new Error(`File upload failed: ${(error as Error).message}`);
     }
