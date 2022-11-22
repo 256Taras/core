411342a4da1969694389d73bf632660bf74c878b
Doc077 <dom.rajkowski@gmail.com>
2022-11-22 20:56:29 +0100

    Improve hook API

--- src/mailer/hooks/use-mail.hook.ts
+++ src/mailer/hooks/use-mail.hook.ts
@@ -2,8 +2,8 @@ import { inject } from '../../injector/functions/inject.function';
 import { Mailer } from '../mailer.class';
 import { MailData } from '../interfaces/mail-data.interface';
 
-export const useMail = (): [(options: MailData) => Promise<string>] => {
+export const useMail = (): (options: MailData) => Promise<string> => {
   const instance = inject(Mailer);
 
-  return [instance.send];
+  return instance.send;
 }
--- src/validator/hooks/use-validator.hook.ts
+++ src/validator/hooks/use-validator.hook.ts
@@ -2,8 +2,8 @@ import { inject } from '../../injector/functions/inject.function';
 import { Validator } from '../validator.class';
 import { ValidationRules } from '../interfaces/validation-rules.interface';
 
-export const useValidator = (): [(rules: Record<string, ValidationRules>, checkOnly: boolean) => boolean] => {
+export const useValidator = (): (rules: Record<string, ValidationRules>, checkOnly: boolean) => boolean => {
   const instance = inject(Validator);
 
-  return [instance.assert];
+  return instance.assert;
 }
