c42f40e1f67affffc02e7886e8d84a9c4e9a3b5b
Doc077 <dom.rajkowski@gmail.com>
2023-01-07 18:17:50 +0100

    Fix CLI options interface

--- src/cli/decorators/command.decorator.ts
+++ src/cli/decorators/command.decorator.ts
@@ -3,15 +3,15 @@ import { Constructor } from '../../utils/interfaces/constructor.interface';
 import { ClassDecorator } from '../../utils/types/class-decorator.type';
 import { Parameter } from '../interfaces/parameter.interface';
 
-interface Data {
-  signature: string;
+interface Options {
+  signature?: string;
   signatures?: string[];
   parameters?: Record<string, Parameter>;
 }
 
-export function Command(data: Data): ClassDecorator {
+export function Command(options: Options): ClassDecorator {
   return (target: Constructor) => {
-    const { signature, signatures, parameters } = data;
+    const { signature, signatures, parameters } = options;
 
     Reflect.defineMetadata('signature', signature, target);
     Reflect.defineMetadata('signatures', signatures, target);
