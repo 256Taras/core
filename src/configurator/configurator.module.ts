03f47668ddfeb3560dd573c0721021f2050bd1f5
Doc077 <dom.rajkowski@gmail.com>
2023-02-17 19:51:43 +0100

    Fix import

--- src/configurator/configurator.module.ts
+++ src/configurator/configurator.module.ts
@@ -1,4 +1,5 @@
 export * from './configurator.service.js';
 export * from './functions/config.function.js';
+export * from './functions/env.function.js';
 export * from './hooks/use-config.hook.js';
 export * from './interfaces/app-config.interface.js';
